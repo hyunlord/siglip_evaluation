a33fb759d1bb8bcb14fcfdc8db766d3c4a9116bf
rexxa <rexxa.som@kakaoenterprise.com>
2025-06-13 03:37:40 +0900

    init

---
+++ main.py
@@ -0,0 +1,86 @@
+from typer import Option, Typer
+from functools import partial
+
+import torch
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from transformers import AutoProcessor, AutoModel
+
+from .util import image_collate_fn, make_text_features, make_image_features, calculate_recall
+
+cmd = Typer()
+
+
+@cmd.command()
+def evaluation(
+        model_path: str = Option(
+            "google/siglip-base-patch16-224"
+            "-m", "--model",
+            help="path of model for evaluation", rich_help_panel="model"
+        ),
+        model_device: str = Option(
+            "cpu",
+            "-md", "--device",
+            help="select device", rich_help_panel="model"
+        ),
+        image_batch_size: int = Option(
+            16,
+            "-ibs", "--image-batch-size",
+            help="image batch size", rich_help_panel="model"
+        ),
+        text_batch_size: int = Option(
+            256,
+            "-tbs", "--text-batch-size",
+            help="text batch size", rich_help_panel="model"
+        ),
+        data_path: str = Option(
+            "hyunlord/aihub_image_caption_enko_dataset",
+            "-d", "--data",
+            help="path of data for evaluation", rich_help_panel="data"
+        ),
+        data_type: str = Option(
+            "test",
+            "-dt", "--data-type",
+            help="data type for evaluation", rich_help_panel="data"
+        ),
+        data_count: int = Option(
+            0,
+            "-dc", "--data-count",
+            help="data count for evaluation", rich_help_panel="data"
+        )
+):
+    device = model_device
+
+    processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True, use_fast=False)
+    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).to(device)
+    model.eval()
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f" 모델 전체 파라미터 수: {total_params:,}")
+
+    dataset = load_dataset(data_path, split=f"{data_type}[:{data_count}]")
+    image_to_text_map = [list(range(i * 5, (i + 1) * 5)) for i in range(len(dataset))]
+    text_to_image_map = [i for i in range(len(dataset)) for _ in range(5)]
+
+    all_captions = [caption for ex in dataset for caption in ex['captions'][:5]]
+    print(f" 데이터셋 캡션 개수: {len(all_captions)}")
+
+    collate_with_processor = partial(image_collate_fn, processor=processor, device=device)
+    image_dataloader = DataLoader(dataset, batch_size=image_batch_size, collate_fn=collate_with_processor, shuffle=False)
+
+    all_text_features = make_text_features(model, processor, all_captions, text_batch_size, device)
+    all_image_features = make_image_features(model, image_dataloader, device)
+    recall_at_1_i2t, recall_at_1_t2i = calculate_recall(model, all_text_features, all_image_features,
+                                                        text_to_image_map, image_to_text_map)
+
+    print("\n" + "=" * 50)
+    print(f"   - Model Path : {model_path})")
+    print(f"   - Data Path : {data_path}), Data Type : {data_type}, Data Count : f{data_count}")
+    print(f"   - 최종 평가 결과")
+    print("=" * 50)
+    print(f"Image-to-Text Recall@1: {recall_at_1_i2t:.2f}%")
+    print(f"Text-to-Image Recall@1: {recall_at_1_t2i:.2f}%")
+    print("=" * 50)
+
+
+if __name__ == '__main__':
+    cmd()
---
+++ util.py
@@ -0,0 +1,68 @@
+from tqdm import tqdm
+
+import torch
+
+
+def image_collate_fn(batch, processor, device):
+    valid_images = []
+    for item in batch:
+        image = item.get('image')
+        if image is None:
+            continue
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+        valid_images.append(image)
+    if not valid_images:
+        return None
+    image_inputs = processor(images=valid_images, return_tensors="pt", padding=True).to(device)
+    return {"pixel_values": image_inputs.pixel_values}
+
+
+def make_text_features(model, processor, texts, batch_size, device):
+    # 텍스트는 양이 많으므로 한 번에 처리
+    with torch.no_grad():
+        print("   - Entire Text Feature 추출...")
+        text_inputs = processor(text=texts,
+                                padding="max_length",
+                                max_length=64,
+                                return_tensors="pt").to(device)
+        text_features_list = []
+        for i in tqdm(range(0, text_inputs.input_ids.shape[0], batch_size), desc="     텍스트 배치 처리"):
+            batch_inputs = {k: v[i:i+batch_size].to(device) for k, v in text_inputs.items()}
+            text_embeds = model.get_text_features(**batch_inputs)
+            text_features_list.append(text_embeds)
+        all_text_features = torch.cat(text_features_list)
+        print("   - Entire Text Feature 준비 완료")
+    return all_text_features
+
+
+def make_image_features(model, image_dataloader, device):
+    all_image_features = []
+    with torch.no_grad():
+        print("   - Entire Image Feature 추출...")
+        for batch in tqdm(image_dataloader, desc="   이미지 배치 처리"):
+            image_embeds = model.get_image_features(pixel_values=batch['pixel_values'].to(device))
+            all_image_features.append(image_embeds)
+        all_image_features = torch.cat(all_image_features)
+        print("   - Entire Image Feature 준비 완료")
+    return all_image_features
+
+
+def calculate_recall(model, all_text_features, all_image_features, text_to_image_map, image_to_text_map):
+    with torch.no_grad():
+        all_image_features /= all_image_features.norm(p=2, dim=-1, keepdim=True)
+        all_text_features /= all_text_features.norm(p=2, dim=-1, keepdim=True)
+
+        logits_per_text = torch.matmul(all_text_features, all_image_features.T)
+        logits_per_text = logits_per_text * model.logit_scale.exp() + model.logit_bias
+        logits_per_image = logits_per_text.t()
+        probs = torch.sigmoid(logits_per_image)
+
+        pred_i2t = probs.argmax(dim=1)
+        correct_i2t = sum(p in it_map for p, it_map in zip(pred_i2t, image_to_text_map))
+        recall_at_1_i2t = (correct_i2t / len(all_image_features)) * 100
+
+        pred_t2i = probs.argmax(dim=0)
+        correct_t2i = sum(p == t_map for p, t_map in zip(pred_t2i, text_to_image_map))
+        recall_at_1_t2i = (correct_t2i / len(all_text_features)) * 100
+    return recall_at_1_i2t, recall_at_1_t2i
